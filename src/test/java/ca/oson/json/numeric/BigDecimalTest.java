2fbe4955003f57c1f2512b1d1f8b7732ea14db25
David He <ruifhe@hotmail.com>
2016-06-24 19:39:54 -0700

    forgot to add test classes

---
+++ src/test/java/ca/oson/json/numeric/BigDecimalTest.java
@@ -0,0 +1,222 @@
+package ca.oson.json.numeric;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import ca.oson.json.Oson.*;
+import ca.oson.json.support.TestCaseBase;
+
+public class BigDecimalTest extends TestCaseBase {
+	   
+	   @Test
+	   public void testSerializeInt() {
+		   BigDecimal value = new BigDecimal(5);
+		   String expected = "5";
+		   
+		   String result = oson.serialize(value);
+		   
+		   assertEquals(expected, result);
+	   }
+	   
+	   
+	   @Test
+	   public void testDeserializeInt() {
+		   BigDecimal value = new BigDecimal(5);
+		   String text = oson.serialize(value);
+		   
+		   BigDecimal result = oson.deserialize(text, BigDecimal.class);
+		   
+		   assertEquals(value, result);
+	   }
+	   
+	   @Test
+	   public void testSerializeIntWithFunction() {
+		   BigDecimal value = new BigDecimal(1);
+		   String expected = "One";
+
+		   oson.setClassMappers(new ClassMapper(BigDecimal.class).setSerializer((BigDecimal p) -> {
+			   switch (p.intValue()) {
+			   case 1: return "One";
+			   case 2: return "Two";
+			   case 3: return "Three";
+			   case 4: return "Four";
+			   case 5: return "Five";
+			   case 6: return "Six";
+			   case 7: return "Seven";
+			   case 8: return "Eight";
+			   case 9: return "Nine";
+			   case 10: return "Ten";
+			   default: return p.toPlainString();
+			   }
+		   }));
+		   
+		   String result = oson.serialize(value);
+		   
+		   assertEquals(expected, result);
+	   }
+	   
+	   
+	   @Test
+	   public void testSerializeIntWithBigDecimal2JsonFunction() {
+		   BigDecimal value = new BigDecimal(6);
+		   String expected = "Six";
+
+		   oson.setClassMappers(new ClassMapper(BigDecimal.class).setSerializer((BigDecimal p) -> {
+			   switch (p.intValue()) {
+			   case 1: return "One";
+			   case 2: return "Two";
+			   case 3: return "Three";
+			   case 4: return "Four";
+			   case 5: return "Five";
+			   case 6: return "Six";
+			   case 7: return "Seven";
+			   case 8: return "Eight";
+			   case 9: return "Nine";
+			   case 10: return "Ten";
+			   default: return p.toString();
+			   }
+		   }));
+		   
+		   String result = oson.serialize(value);
+		   
+		   assertEquals(expected, result);
+	   }
+	   
+	   
+	   @Test
+	   public void testSerializeIntWithBigDecimal2JsonFunctionDirect() {
+		   BigDecimal value = new BigDecimal(8);
+		   String expected = "Eight";
+
+		   oson.setClassMappers(new ClassMapper(BigDecimal.class).setSerializer(
+				   (BigDecimal p) -> {
+			   switch (p.intValue()) {
+			   case 1: return "One";
+			   case 2: return "Two";
+			   case 3: return "Three";
+			   case 4: return "Four";
+			   case 5: return "Five";
+			   case 6: return "Six";
+			   case 7: return "Seven";
+			   case 8: return "Eight";
+			   case 9: return "Nine";
+			   case 10: return "Ten";
+			   default: return p.toPlainString();
+			   }
+		   }));
+		   
+		   String result = oson.serialize(value);
+		   
+		   assertEquals(expected, result);
+	   }
+	   
+	   
+	   @Test
+	   public void testDeserializeIntWithFunction() {
+		   String value = "Seven";
+		   BigDecimal expected = new BigDecimal (7);
+
+		   oson.setClassMappers(new ClassMapper(BigDecimal.class)
+		   	.setDeserializer((String p) -> {
+		   			int val;
+				   switch (p) {
+				   case "One": val = 1; break;
+				   case "Two": val = 2; break;
+				   case "Three": val = 3; break;
+				   case "Four": val = 4; break;
+				   case "Five": val = 5; break;
+				   case "Six": val = 6; break;
+				   case "Seven": val = 7; break;
+				   case "Eight": val = 8; break;
+				   case "Nine": val = 9; break;
+				   case "Ten": val = 10; break;
+				   default: val = Integer.parseInt(p); break;
+				   }
+				   return new BigDecimal(val);
+			   }));
+
+		   BigDecimal result = oson.deserialize(value, BigDecimal.class);
+		   
+		   assertEquals(expected, result);
+	   }
+
+	   @Test
+	   public void testSerializeIntWithFunctionEnum() {
+		   BigDecimal value = new BigDecimal (1);
+		   String expected = "UNDERSCORE_UPPER_CAMELCASE";
+		   
+		   oson.setClassMappers(new ClassMapper(BigDecimal.class)
+		   	.setSerializer((Object p) -> FIELD_NAMING.UNDERSCORE_UPPER_CAMELCASE));
+		   
+		   String result = oson.serialize(value);
+
+		   assertEquals(expected, result);
+	   } 
+	   
+	   @Test
+	   public void testDeserializeIntWithFunctionEnum() {
+		   String value = "1";
+		   BigDecimal expected = new BigDecimal(4);
+		   
+		   oson.setClassMappers(new ClassMapper(BigDecimal.class)
+		   	.setDeserializer((Object p) -> FIELD_NAMING.UNDERSCORE_UPPER_CAMELCASE));
+		   
+		   BigDecimal result = oson.deserialize(value, BigDecimal.class);
+
+		   assertEquals(expected, result);
+	   }
+	   
+	   @Test
+	   public void testSerializeIntWithMin() {
+		   BigDecimal value = new BigDecimal(1);
+		   Integer min = 10;
+		   String expected = min + "";
+		   
+		   oson.setClassMappers(new ClassMapper(BigDecimal.class).setMin(min));
+		   
+		   String result = oson.serialize(value);
+
+		   assertEquals(expected, result);
+	   }
+	   
+	   @Test
+	   public void testSerializeIntWithMax() {
+		   BigDecimal value = new BigDecimal(1000000000);
+		   Integer max = 2000;
+		   String expected = max + "";
+		   
+		   oson.setClassMappers(new ClassMapper(BigDecimal.class).setMax(max));
+		   
+		   String result = oson.serialize(value);
+
+		   assertEquals(expected, result);
+	   }
+	   
+	   @Test
+	   public void testDeserializeIntWithMin() {
+		   String value = "1";
+		   Integer min = 10;
+		   BigDecimal expected = new BigDecimal(min);
+		   
+		   oson.setClassMappers(new ClassMapper(BigDecimal.class).setMin(min));
+		   
+		   BigDecimal result = oson.deserialize(value, BigDecimal.class);
+
+		   assertEquals(expected, result);
+	   }
+	   
+	   @Test
+	   public void testDeserializeIntWithMax() {
+		   String value = "19999999";
+		   Integer max = 10000;
+		   BigDecimal expected = new BigDecimal(max);
+		   
+		   oson.setClassMappers(new ClassMapper(BigDecimal.class).setMax(max));
+		   
+		   BigDecimal result = oson.deserialize(value, BigDecimal.class);
+
+		   assertEquals(expected, result);
+	   }
+	   
+}
---
+++ src/test/java/ca/oson/json/numeric/BigIntegerTest.java
@@ -0,0 +1,222 @@
+package ca.oson.json.numeric;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+import ca.oson.json.Oson.*;
+import ca.oson.json.support.TestCaseBase;
+
+public class BigIntegerTest extends TestCaseBase {
+	   
+	   @Test
+	   public void testSerializeInt() {
+		   BigInteger value = BigInteger.valueOf(5);
+		   String expected = "5";
+		   
+		   String result = oson.serialize(value);
+		   
+		   assertEquals(expected, result);
+	   }
+	   
+	   
+	   @Test
+	   public void testDeserializeInt() {
+		   BigInteger value = BigInteger.valueOf(5);
+		   String text = oson.serialize(value);
+		   
+		   BigInteger result = oson.deserialize(text, BigInteger.class);
+		   
+		   assertEquals(value, result);
+	   }
+	   
+	   @Test
+	   public void testSerializeIntWithFunction() {
+		   BigInteger value = BigInteger.valueOf(1);
+		   String expected = "One";
+
+		   oson.setClassMappers(new ClassMapper(BigInteger.class).setSerializer((BigInteger p) -> {
+			   switch (p.intValue()) {
+			   case 1: return "One";
+			   case 2: return "Two";
+			   case 3: return "Three";
+			   case 4: return "Four";
+			   case 5: return "Five";
+			   case 6: return "Six";
+			   case 7: return "Seven";
+			   case 8: return "Eight";
+			   case 9: return "Nine";
+			   case 10: return "Ten";
+			   default: return p.toString();
+			   }
+		   }));
+		   
+		   String result = oson.serialize(value);
+		   
+		   assertEquals(expected, result);
+	   }
+	   
+	   
+	   @Test
+	   public void testSerializeIntWithBigInteger2JsonFunction() {
+		   BigInteger value = BigInteger.valueOf(6);
+		   String expected = "Six";
+
+		   oson.setClassMappers(new ClassMapper(BigInteger.class).setSerializer((BigInteger p) -> {
+			   switch (p.intValue()) {
+			   case 1: return "One";
+			   case 2: return "Two";
+			   case 3: return "Three";
+			   case 4: return "Four";
+			   case 5: return "Five";
+			   case 6: return "Six";
+			   case 7: return "Seven";
+			   case 8: return "Eight";
+			   case 9: return "Nine";
+			   case 10: return "Ten";
+			   default: return p.toString();
+			   }
+		   }));
+		   
+		   String result = oson.serialize(value);
+		   
+		   assertEquals(expected, result);
+	   }
+	   
+	   
+	   @Test
+	   public void testSerializeIntWithBigInteger2JsonFunctionDirect() {
+		   BigInteger value = BigInteger.valueOf(8);
+		   String expected = "Eight";
+
+		   oson.setClassMappers(new ClassMapper(BigInteger.class).setSerializer(
+				   (BigInteger p) -> {
+			   switch (p.intValue()) {
+			   case 1: return "One";
+			   case 2: return "Two";
+			   case 3: return "Three";
+			   case 4: return "Four";
+			   case 5: return "Five";
+			   case 6: return "Six";
+			   case 7: return "Seven";
+			   case 8: return "Eight";
+			   case 9: return "Nine";
+			   case 10: return "Ten";
+			   default: return p.toString();
+			   }
+		   }));
+		   
+		   String result = oson.serialize(value);
+		   
+		   assertEquals(expected, result);
+	   }
+	   
+	   
+	   @Test
+	   public void testDeserializeIntWithFunction() {
+		   String value = "Seven";
+		   BigInteger expected = BigInteger.valueOf (7);
+
+		   oson.setClassMappers(new ClassMapper(BigInteger.class)
+		   	.setDeserializer((String p) -> {
+		   			int val;
+				   switch (p) {
+				   case "One": val = 1; break;
+				   case "Two": val = 2; break;
+				   case "Three": val = 3; break;
+				   case "Four": val = 4; break;
+				   case "Five": val = 5; break;
+				   case "Six": val = 6; break;
+				   case "Seven": val = 7; break;
+				   case "Eight": val = 8; break;
+				   case "Nine": val = 9; break;
+				   case "Ten": val = 10; break;
+				   default: val = Integer.parseInt(p); break;
+				   }
+				   return BigInteger.valueOf(val);
+			   }));
+
+		   BigInteger result = oson.deserialize(value, BigInteger.class);
+		   
+		   assertEquals(expected, result);
+	   }
+
+	   @Test
+	   public void testSerializeIntWithFunctionEnum() {
+		   BigInteger value = BigInteger.valueOf (1);
+		   String expected = "UNDERSCORE_UPPER_CAMELCASE";
+		   
+		   oson.setClassMappers(new ClassMapper(BigInteger.class)
+		   	.setSerializer((Object p) -> FIELD_NAMING.UNDERSCORE_UPPER_CAMELCASE));
+		   
+		   String result = oson.serialize(value);
+
+		   assertEquals(expected, result);
+	   } 
+	   
+	   @Test
+	   public void testDeserializeIntWithFunctionEnum() {
+		   String value = "1";
+		   BigInteger expected = BigInteger.valueOf(4);
+		   
+		   oson.setClassMappers(new ClassMapper(BigInteger.class)
+		   	.setDeserializer((Object p) -> FIELD_NAMING.UNDERSCORE_UPPER_CAMELCASE));
+		   
+		   BigInteger result = oson.deserialize(value, BigInteger.class);
+
+		   assertEquals(expected, result);
+	   }
+	   
+	   @Test
+	   public void testSerializeIntWithMin() {
+		   BigInteger value = BigInteger.valueOf(1);
+		   Integer min = 10;
+		   String expected = min + "";
+		   
+		   oson.setClassMappers(new ClassMapper(BigInteger.class).setMin(min));
+		   
+		   String result = oson.serialize(value);
+
+		   assertEquals(expected, result);
+	   }
+	   
+	   @Test
+	   public void testSerializeIntWithMax() {
+		   BigInteger value = BigInteger.valueOf(1000000000);
+		   Integer max = 2000;
+		   String expected = max + "";
+		   
+		   oson.setClassMappers(new ClassMapper(BigInteger.class).setMax(max));
+		   
+		   String result = oson.serialize(value);
+
+		   assertEquals(expected, result);
+	   }
+	   
+	   @Test
+	   public void testDeserializeIntWithMin() {
+		   String value = "1";
+		   Integer min = 10;
+		   BigInteger expected = BigInteger.valueOf(min);
+		   
+		   oson.setClassMappers(new ClassMapper(BigInteger.class).setMin(min));
+		   
+		   BigInteger result = oson.deserialize(value, BigInteger.class);
+
+		   assertEquals(expected, result);
+	   }
+	   
+	   @Test
+	   public void testDeserializeIntWithMax() {
+		   String value = "19999999";
+		   Integer max = 10000;
+		   BigInteger expected = BigInteger.valueOf(max);
+		   
+		   oson.setClassMappers(new ClassMapper(BigInteger.class).setMax(max));
+		   
+		   BigInteger result = oson.deserialize(value, BigInteger.class);
+
+		   assertEquals(expected, result);
+	   }
+	   
+}
